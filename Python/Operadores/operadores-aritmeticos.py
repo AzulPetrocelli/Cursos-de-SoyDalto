119d9ae459091de5a8cc9b8485586df5a799f893
Azul Petrocelli <azulpetrocelli81@gmail.com>
2025-05-20 16:04:01 -0300

    operadores aritmeticos (Python)

---
+++ Python/Operadores/operadores-aritmeticos.py
@@ -0,0 +1,18 @@
+#Suma y Resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#Multiplciacion y divicion (* y /) (la divicion devuelve un decimal)
+
+#Potenciacion (**) (exponente)
+exponente = 12 ** 5
+
+#division Baja(//) (devuelve entero redondeado hacia abajo ej 2.9 = 2)
+division_baja = 12 // 5
+
+#resto o modulo (%)
+resto = 12 % 5
+
+#type(dato) nos devuelve que tipo de dato es
+tipo_de_dato = type(5) 
+print(tipo_de_dato)
