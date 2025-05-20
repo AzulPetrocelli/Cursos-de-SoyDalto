173c08eaa1fac5ddd5bac3d7fb2c46f99dfc91ee
Azul Petrocelli <azulpetrocelli81@gmail.com>
2025-05-20 13:53:34 -0300

    Tipos de datos

---
+++ Python/Tipos de datos/tipos-de-datos.py
@@ -0,0 +1,16 @@
+"esta nomenclatura nos sirve para textos de una linea"
+'esta nomenclatura nos sirve para textos de una linea'
+
+"""esta nomenclatura nos sirve
+para textos con saltos de linea"""
+
+'''esta nomenclatura nos sirve
+para textos con saltos de linea'''
+
+"Los flotantes van con . no con ,"
+4
+4.4
+
+"Booleanos, tienen que arrancar en mayuscula"
+True
+False
