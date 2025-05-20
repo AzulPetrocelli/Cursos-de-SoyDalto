824fd90cb03d98d48a4446a02ebb789945143ea5
Azul Petrocelli <azulpetrocelli81@gmail.com>
2025-05-20 16:11:58 -0300

    operadores de comparacion (Python)

---
+++ Python/Operadores/operadores_de_comparacion.py
@@ -0,0 +1,18 @@
+igual_que = 5 == 6
+print(igual_que)#False
+
+distinto_de = 5 != 6
+print(distinto_de)#True
+
+mayor_que = 5 > 6
+print(mayor_que)#False
+
+menor_que = 5 < 6
+print(menor_que)#True
+
+mayor_o_igual = 5 >= 6
+print(mayor_o_igual)#False
+
+menor_o_igual = 5 <= 6
+print(menor_o_igual)#True
+
