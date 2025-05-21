f183ec212371873eb6a09a53efb410a2f96b3dbb
Azul Petrocelli <azulpetrocelli81@gmail.com>
2025-05-21 00:35:39 -0300

    bucles con listas y tuplas (Python)

---
+++ Python/Bucles/iterar_listas_y_tuplas.py
@@ -0,0 +1,36 @@
+#Todo lo que vamos a hacer funciona tanto con listas
+# como con tuplas
+
+#Lista de mascotas
+mascotas = ["gato", "perro", "loro", "pez"]
+
+#Tupla con edades
+edades = (3, 5, 2, 1)
+
+#Ciclo for
+for animal in mascotas:
+    print(f"Ahora la variable animal es igual a: {animal}")
+    
+#Iterar dos o mas listas y/o tuplas del mismo tamaño al mismo tiempo
+for animal,edad in zip(mascotas,edades):
+    print(f"Mi {animal} tiene {edad} años de edad")
+
+#Iterar elementos 5 veces
+for num in range(5):
+    print(num)
+
+#Iterar elementos en un rango definido de 10 a 20
+for num in range(10,20):
+    print(num)
+    
+#Recorrer una lista con su indice
+for animal in enumerate(mascotas): #animal es una tupla --> (indice , valor)
+    indice = animal[0]
+    mascota = animal[1]
+    print(f"El indice del {mascota} en la lista de mascotas es {indice}")
+
+#Usando el else (siempre se ejecuta luego del for)
+for animal in mascotas:
+    print(f"Tengo un {animal} de mascota")
+else:
+    print(f"Esas son todas mis mascotas el ultimo nombrado fue el {animal}")
