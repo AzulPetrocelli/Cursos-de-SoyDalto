c9666cda3339d7aa06ea30635f81466922248a64
Azul Petrocelli <azulpetrocelli81@gmail.com>
2025-05-28 16:33:16 -0300

    Clases y objetos (Python)

---
+++ POO con Python/clases_y_objetos.py
@@ -0,0 +1,14 @@
+#Creo una clase
+class Celular():
+    #Atributos estaticos (Igual para todas las instancias)
+    marca = "Samsung"
+    modelo = "S23" 
+    camara = "48MP"
+
+#Creo el objeto instanciando la clase
+celular1 = Celular()
+
+#Muestro los atributos de la clase
+print("celular1.marca")
+print("celular1.modelo")
+print("celular1.camara")
